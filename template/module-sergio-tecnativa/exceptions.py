ff8420d13f83f5fcfcdc86214f91a26f0fbbca99
sergio-incaser <sergio@incaser.es>
2016-05-01 12:17:54 +0200

    [8.0] Template addon sergio teruel Tecnativa

---
+++ template/module-sergio-tecnativa/exceptions.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import _, exceptions
+
+
+class ModuleNameValidationError(exceptions.ValidationError):
+    """Base class for this module's validation errors."""
+    def __init__(self, *args, **kwargs):
+        self._args, self._kwargs = args, kwargs
+        value = self._message()
+        super(ModuleNameValidationError, self).__init__(value)
+
+    def _message(self):
+        """Format the message."""
+        return self.__doc__.format(*self._args, **self._kwargs)
+
+
+class WrongNameError(ModuleNameValidationError):
+    """Name {} is wrong."""
+
+
+class TranslatedWrongNameError(ModuleNameValidationError):
+    __doc__ = _("Name {} is wrong.")
---
+++ template/module-sergio-tecnativa/migrations/8.0.1.0.0/post-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+def migrate(cr, version):
+    """Update database from previous versions, after updating module."""
+    pass
