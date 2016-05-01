468e6444fdd49ae13e61fa6ce30a53b7d49be1e9
sergio-incaser <sergio@incaser.es>
2016-05-01 12:26:22 +0200

    [8.0] Template addon sergio teruel Tecnativa

--- template/module-sergio-tecnativa/__openerp__.py
+++ template/module-sergio-tecnativa/__openerp__.py
@@ -6,8 +6,9 @@
     "summary": "Module summary",
     "version": "8.0.1.0.0",
     "category": "Uncategorized",
-    "website": "https://odoo-community.org/",
-    "author": "<AUTHOR(S)>, Odoo Community Association (OCA)",
+    'website': 'http://www.tecnativa.com',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
     "license": "AGPL-3",
     "application": False,
     "installable": True,
